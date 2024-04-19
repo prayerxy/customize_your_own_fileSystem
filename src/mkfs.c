d4cc19427a6d0e53fd85b16221ad3f38cb5d3f89
zjremo <3106316195@qq.com>
2024-04-19 22:56:05 +0800

    提交架构

---
+++ src/mkfs.c
@@ -0,0 +1,15 @@
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <linux/fs.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include <errno.h>
+
+#include "../include/XCraft.h"
+
