daafb04c35b04bc6f61c3c41053e95429bf7d4fd
prayerxy <1401662806@qq.com>
2024-07-29 18:15:38 +0800

    更新

--- src/super.c
+++ src/super.c
@@ -8,6 +8,8 @@
 #include <linux/statfs.h>
 #include "XCraft.h"
 
+//有一些通用函数放在这里
+
 
 // slab cache for XCraft_inode_info
 static struct kmem_cache *XCraft_inode_cache;
@@ -50,82 +52,6 @@ void XCraft_destroy_inode(struct inode *inode)
 {
     struct XCraft_inode_info *xi = XCRAFT_I(inode);
     kmem_cache_free(XCraft_inode_cache, xi);
-}
-
-
-
-struct XCraft_group_desc* get_group_desc(struct XCraft_superblock_info *sbi, xcraft_group_t group, struct buffer_head *bh)
-{
-    struct XCraft_group_desc* desc = NULL;
-    uint32_t block = group / sbi->s_desc_per_block;
-    uint32_t offset = group % sbi->s_desc_per_block;
-    struct buffer_head **s_group_desc;
-    s_group_desc = sbi->s_group_desc;
-    bh = s_group_desc[block];
-    desc=(struct XCraft_group_desc *)(bh->b_data);
-    desc += offset;
-    return desc;
-}
-struct XCraft_group_desc* get_group_desc2(struct XCraft_superblock_info *sbi,xcraft_group_t group)
-{
-    struct XCraft_group_desc* desc = NULL;
-    uint32_t block = group / sbi->s_desc_per_block;
-    uint32_t offset = group % sbi->s_desc_per_block;
-    desc=(struct XCraft_group_desc *)(sbi->s_group_desc[block])->b_data;
-    desc += offset;
-    return desc;
-}
-int group_free_inodes_count(struct XCraft_superblock_info *sbi,struct XCraft_group_desc* desc)
-{
-    return le16_to_cpu(desc->bg_free_inodes_count);
-}
-
-int group_free_blocks_count(struct XCraft_superblock_info *sbi,struct XCraft_group_desc* desc)
-{
-    return le16_to_cpu(desc->bg_free_blocks_count);
-}
-
-int inode_get_block_group(struct XCraft_superblock_info *sb_info, uint32_t ino)
-{   
-    struct XCraft_superblock *disk_sb=sb_info->s_super;
-    uint32_t tmp;
-    tmp = le32_to_cpu(disk_sb->s_inodes_count)-(sb_info->s_last_group_inodes);
-    if(ino>=tmp)
-        return sb_info->s_groups_count-1;
-    else
-        return ino/(sb_info->s_inodes_per_group);
-    
-}
-
-int inode_get_block_group_shift(struct XCraft_superblock_info *sb_info, uint32_t ino)
-{
-    struct XCraft_superblock *disk_sb=sb_info->s_super;
-    uint32_t tmp;
-    tmp = le32_to_cpu(disk_sb->s_inodes_count)-(sb_info->s_last_group_inodes);
-    if(ino>=tmp)
-        return ino-tmp;
-    else
-        return ino%(sb_info->s_inodes_per_group);
-}
-int get_block_group(struct XCraft_superblock_info *sb_info, uint32_t block)
-{
-    struct XCraft_superblock *disk_sb=sb_info->s_super;
-    uint32_t tmp;
-    tmp = le32_to_cpu(disk_sb->s_blocks_count)-(sb_info->s_last_group_blocks);
-    if(block>=tmp)
-        return sb_info->s_groups_count-1;
-    else
-        return block/(sb_info->s_blocks_per_group);
-}
-int get_block_group_shift(struct XCraft_superblock_info *sb_info, uint32_t block)
-{
-    struct XCraft_superblock *disk_sb=sb_info->s_super;
-    uint32_t tmp;
-    tmp = le32_to_cpu(disk_sb->s_blocks_count)-(sb_info->s_last_group_blocks);
-    if(block>=tmp)
-        return block-tmp;
-    else
-        return block%(sb_info->s_blocks_per_group);
 }
 
 
